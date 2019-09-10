a70b6f0bcf9a7f8860efd8f3814567e90ad53125
adkhiker <poirier.greg@gmail.com>
2019-09-10 16:57:37 -0400

    WIP Operators

--- src/components/ButtonComponents/OperatorButtons/OperatorButton.js
+++ src/components/ButtonComponents/OperatorButtons/OperatorButton.js
@@ -1,9 +1,16 @@
 import React from "react";
 
-const OperatorButton = () => {
+const OperatorButton = ({ operator, value }) => {
+  const operatorStyle = {
+    background: "#007497",
+    color: "white"
+  };
   return (
     <>
       {/* Display a button element rendering the data being passed down from the parent container on props */}
+      <button style={operatorStyle}>{operator}</button>
     </>
   );
 };
+
+export default OperatorButton;
